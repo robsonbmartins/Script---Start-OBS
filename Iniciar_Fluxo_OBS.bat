3975404121bd8551910cf751a2d489b8a89e8193
Robson Martins <46879602+robsonbmartins@users.noreply.github.com>
2026-03-31 09:37:21 -0300

    fix: remove accent from comment in batch script for better encoding compatibility

--- Iniciar_Fluxo_OBS.bat
+++ Iniciar_Fluxo_OBS.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Este arquivo batch facilita a inicializacao do script PowerShell principal
-REM Isso previne qualquer problema de "ExecutionPolicy" no Windows e serve como um atalho fácil.
+REM Isso previne qualquer problema de "ExecutionPolicy" no Windows e serve como um atalho facil.
 
 TITLE Inicializador do Fluxo OBS
 SET "ScriptPath=%~dp0Iniciar_Fluxo_OBS.ps1"
